4b12b4643a78c5c8a3b91c955010f613ef22f622
Zack Goldblum <zackgoldblum@gmail.com>
2023-01-06 11:54:34 -0500

    Improve docstrings and function definitions

--- central/connect_and_recv_uart.py
+++ central/connect_and_recv_uart.py
@@ -5,21 +5,21 @@ from discover_devices import discover_devices
 RX_UUID = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"  # UART receive data UUID
 
 def handle_RX(sender: int, data: bytearray) -> None: 
-    """ Called whenever BLE data is received
+    """ Called whenever BLE data is received.
 
     Args:
-        sender (int): BLE device handle
-        data (bytearray): Data received from BLE device
+        sender (int): BLE device handle.
+        data (bytearray): Data received from BLE device.
 
     """
     data = data.decode("utf-8")
     print(f"Received: {data}")
 
 async def handle_disconnect(client: bleak.BleakClient) -> None:
-    """ Disconnect from BLE device
+    """ Disconnect from BLE device.
 
     Args:
-        client (bleak.BleakClient): _description_
+        client (bleak.BleakClient): BLE device client.
     """
     await client.disconnect()
 
@@ -27,7 +27,7 @@ async def connect_and_recv(device: bleak.backends.device.BLEDevice, recv_time: i
     """ Connect to BLE device and receive data.
 
     Args:
-        device (bleak.backends.device.BLEDevice): BLE device
+        device (bleak.backends.device.BLEDevice): BLE device.
         recv_time (int, optional): Number of seconds of BLE data to receive. Defaults to 10.
     """
     print(f"Attempting to connect to {device.address}")
@@ -41,8 +41,14 @@ async def connect_and_recv(device: bleak.backends.device.BLEDevice, recv_time: i
     await handle_disconnect(client)
     print("Disconnected from", device.address)
 
-async def main(devices: list) -> None:
-    await asyncio.gather(*[connect_and_recv(device) for device in devices])
+async def main(devices: list, recv_time: int = 10) -> None:
+    """ Entrypoint into the script.
+
+    Args:
+        devices (list): BLE devices to connect to and receive data from.
+        recv_time (int, optional): Number of seconds of BLE data to receive. Defaults to 10.
+    """
+    await asyncio.gather(*[connect_and_recv(device, recv_time) for device in devices])
 
 # Example usage:
 # --------------
