ee0a86ada6de23f1d83325f9b89d40a2caa8340a
Zack Goldblum <zackgoldblum@gmail.com>
2023-01-06 13:05:33 -0500

    Create serial_to_device.py

---
+++ central/serial_to_device.py
@@ -0,0 +1,53 @@
+import time
+import serial
+
+def get_COM_port() -> str:
+    """ Get COM port of the BLE device.
+
+    Returns:
+        str: COM port.
+    """
+    COM_num = input("Enter COM number: ")
+    COM_port = f"COM{COM_num}"
+    return COM_port
+
+def write_to_device(output_type: str, ser: serial.Serial) -> None:
+    """ Send data to the BLE device via serial.
+
+    Args:
+        output_type (str): "loop" or "manual".
+        ser (serial.Serial): Serial port connection.
+    """
+    if output_type == "loop":
+        i = 0
+        while True:
+            msg = f"{i} - Hello!"
+            ser.write(msg.encode())
+            i += 1
+            time.sleep(1)
+            print(f"Sent '{msg}'")
+    elif output_type == "manual":
+        while True:
+            msg = input("Enter your message: ")
+            ser.write(msg.encode())
+            print(f"Sent '{msg}'")
+
+def main() -> None:
+    """ Entrypoint into the script.
+    """
+    COM_port = get_COM_port()
+    ser = None
+    while not ser:
+        try:
+            ser = serial.Serial(COM_port, 9600, timeout=0.5)
+        except serial.SerialException as e:
+            print("Invalid COM port. Please try again.")
+            COM_port = get_COM_port()
+
+    output_type = None
+    while output_type not in ("loop", "manual"):
+        output_type = str(input("Enter output type ('loop' or 'manual'): "))
+    write_to_device(output_type, ser)
+
+if __name__ == "__main__":
+    main()
