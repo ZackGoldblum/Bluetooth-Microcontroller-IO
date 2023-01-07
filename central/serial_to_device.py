4ebe1404774d5d15c16f082c2e11b704e609fbed
Zack Goldblum <zackgoldblum@gmail.com>
2023-01-07 18:46:07 -0500

    Update serial_to_device.py

--- central/serial_to_device.py
+++ central/serial_to_device.py
@@ -11,6 +11,17 @@ def get_COM_port() -> str:
     COM_port = f"COM{COM_num}"
     return COM_port
 
+def out_packet(msg: str) -> bytearray:
+    """ Convert input message into a serial packet.
+
+    Args:
+        msg (str): Message to send to the device.
+
+    Returns:
+        bytearray: Data packet to output via serial.
+    """
+    return f"{msg}\r".encode("utf-8")
+
 def write_to_device(output_type: str, ser: serial.Serial) -> None:
     """ Send data to the BLE device via serial.
 
@@ -23,15 +34,16 @@ def write_to_device(output_type: str, ser: serial.Serial) -> None:
             i = 0
             while ser:
                 msg = f"{i} - Hello!"
-                ser.write(msg.encode())
+                ser.write(out_packet(msg))
                 i += 1
                 time.sleep(1)
                 print(f"Sent '{msg}'")
         elif output_type == "manual":
             while ser:
                 msg = input("Enter your message: ")
-                ser.write(msg.encode())
+                ser.write(out_packet(msg))
                 print(f"Sent '{msg}'")
+                data = ser.readline().decode().strip()
     except serial.SerialException:
         print("Serial connection closed.")
     except KeyboardInterrupt:
@@ -51,24 +63,11 @@ def main() -> None:
             print("Invalid COM port. Please try again.")
             COM_port = get_COM_port()
 
-    ser.write("serial_conn\r".encode())
-    timeout = time.time() + 3  # 3-second timeout if no response from device
-
-    while not serial_connected:  # listen for response from device
-        data = ser.readline().decode().strip()
-        if data == "connected":
-            print("Serial connection established.")
-            serial_connected = True
-        elif time.time() > timeout:
-            print("Failed to establish serial connection.")
-            break
-
-    if serial_connected:  # enter messaging interface
-        output_type = None
-        while output_type not in ("loop", "manual"):
-            output_type = str(input("Enter output type ('loop' or 'manual'): "))
-        print("Press CTRL+C to end.")
-        write_to_device(output_type, ser)
+    output_type = None
+    while output_type not in ("loop", "manual"):
+        output_type = str(input("Enter output type ('loop' or 'manual'): "))
+    print("Press CTRL+C to end.")
+    write_to_device(output_type, ser)
 
 if __name__ == "__main__":
     main()
